ad5af60c5ba3d8fb2b6ec66d97c6329d4b766549
RahulGupta1910 <rahulsuperp007@gmail.com>
2023-10-20 15:00:52 +0530

    heading changes

---
+++ main.js
@@ -0,0 +1,8 @@
+let cursor = document.querySelector(".cursor");
+
+let main = document.querySelector(".main");
+
+main.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.x + "px";
+  cursor.style.top = e.y + "px";
+});
